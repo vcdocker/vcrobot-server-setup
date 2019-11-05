40c4535991a5b2b5681b3a67099e9bfe5e0792c4
hieupv <hieupv@codersvn.com>
2019-11-05 17:14:22 +0700

    update

--- init.sh
+++ init.sh
@@ -9,15 +9,15 @@ curl -L https://github.com/vcdocker/docker-traefik/archive/2.0.0.tar.gz | tar -x
 docker swarm init
 docker network create --driver=overlay --attachable vcrobot
 
-echo 'export VC_USER=admin' >> ~/.bash_profile && export VC_USER=admin
-echo 'export VC_PASSWORD=secret' >> ~/.bash_profile && export VC_PASSWORD=secret
-echo 'export VC_BASIC_AUTH=$(htpasswd -nb $VC_USER $VC_PASSWORD)' >> ~/.bash_profile && export VC_BASIC_AUTH=$(htpasswd -nb $VC_USER $VC_PASSWORD)
-echo 'export ACME_EMAIL=dev@vicoders.com' >> ~/.bash_profile && export ACME_EMAIL=dev@vicoders.com
-echo 'export TRAEFIK_DEBUGE=true' >> ~/.bash_profile && export TRAEFIK_DEBUGE=true
-echo 'export ENABLE_TRAEFIK_API=true' >> ~/.bash_profile && export ENABLE_TRAEFIK_API=true
-echo 'export ENABLE_TRAEFIK_API_DASHBOARD=true' >> ~/.bash_profile && export ENABLE_TRAEFIK_API_DASHBOARD=true
-echo 'export ENABLE_TRAEFIK_CERT_RESOLVER=staging' >> ~/.bash_profile && export ENABLE_TRAEFIK_CERT_RESOLVER=staging
-echo 'export ENABLE_TRAEFIK_ACCESS_LOG=true' >> ~/.bash_profile && export ENABLE_TRAEFIK_ACCESS_LOG=true
+echo 'export VC_USER=admin' >> ~/.bashrc && export VC_USER=admin
+echo 'export VC_PASSWORD=secret' >> ~/.bashrc && export VC_PASSWORD=secret
+echo 'export VC_BASIC_AUTH=$(htpasswd -nb $VC_USER $VC_PASSWORD)' >> ~/.bashrc && export VC_BASIC_AUTH=$(htpasswd -nb $VC_USER $VC_PASSWORD)
+echo 'export ACME_EMAIL=dev@vicoders.com' >> ~/.bashrc && export ACME_EMAIL=dev@vicoders.com
+echo 'export TRAEFIK_DEBUGE=true' >> ~/.bashrc && export TRAEFIK_DEBUGE=true
+echo 'export ENABLE_TRAEFIK_API=true' >> ~/.bashrc && export ENABLE_TRAEFIK_API=true
+echo 'export ENABLE_TRAEFIK_API_DASHBOARD=true' >> ~/.bashrc && export ENABLE_TRAEFIK_API_DASHBOARD=true
+echo 'export ENABLE_TRAEFIK_CERT_RESOLVER=staging' >> ~/.bashrc && export ENABLE_TRAEFIK_CERT_RESOLVER=staging
+echo 'export ENABLE_TRAEFIK_ACCESS_LOG=true' >> ~/.bashrc && export ENABLE_TRAEFIK_ACCESS_LOG=true
 
 touch letsencrypt/acme.json && chmod 600 letsencrypt/acme.json
 touch logs/access.log
