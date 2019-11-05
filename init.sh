66c4e67a048ec652c4e5c188ecc54dfd0ec03575
hieupv <hieupv@codersvn.com>
2019-11-05 17:05:19 +0700

    fix init command

--- init.sh
+++ init.sh
@@ -9,15 +9,15 @@ curl -L https://github.com/vcdocker/docker-traefik/archive/2.0.0.tar.gz | tar -x
 docker swarm init
 docker network create --driver=overlay --attachable vcrobot
 
-export VC_USER=admin
-export VC_PASSWORD=secret
-export VC_BASIC_AUTH=$(htpasswd -nb $VC_USER $VC_PASSWORD)
-export ACME_EMAIL=dev@vicoders.com
-export TRAEFIK_DEBUGE=true
-export ENABLE_TRAEFIK_API=true
-export ENABLE_TRAEFIK_API_DASHBOARD=true
-export ENABLE_TRAEFIK_CERT_RESOLVER=staging
-export ENABLE_TRAEFIK_ACCESS_LOG=true
+echo 'export VC_USER=admin' >> ~/.bash_profile && export VC_USER=admin
+echo 'export VC_PASSWORD=secret' >> ~/.bash_profile && export VC_PASSWORD=secret
+echo 'export VC_BASIC_AUTH=$(htpasswd -nb $VC_USER $VC_PASSWORD)' >> ~/.bash_profile && export VC_BASIC_AUTH=$(htpasswd -nb $VC_USER $VC_PASSWORD)
+echo 'export ACME_EMAIL=dev@vicoders.com' >> ~/.bash_profile && export ACME_EMAIL=dev@vicoders.com
+echo 'export TRAEFIK_DEBUGE=true' >> ~/.bash_profile && export TRAEFIK_DEBUGE=true
+echo 'export ENABLE_TRAEFIK_API=true' >> ~/.bash_profile && export ENABLE_TRAEFIK_API=true
+echo 'export ENABLE_TRAEFIK_API_DASHBOARD=true' >> ~/.bash_profile && export ENABLE_TRAEFIK_API_DASHBOARD=true
+echo 'export ENABLE_TRAEFIK_CERT_RESOLVER=staging' >> ~/.bash_profile && export ENABLE_TRAEFIK_CERT_RESOLVER=staging
+echo 'export ENABLE_TRAEFIK_ACCESS_LOG=true' >> ~/.bash_profile && export ENABLE_TRAEFIK_ACCESS_LOG=true
 
 touch letsencrypt/acme.json && chmod 600 letsencrypt/acme.json
 touch logs/access.log
