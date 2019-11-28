a2e3783aa63456a1b0c3243f862740db1ebe0a6e
hieupv <hieupv@codersvn.com>
2019-11-28 11:00:57 +0700

    update

--- init.sh
+++ init.sh
@@ -11,15 +11,15 @@ docker network create --driver=overlay --attachable vcrobot
 
 sudo apt install apache2-utils -y
 
-echo 'export VC_USER=admin' >> ~/.bashrc && export VC_USER=admin
-echo 'export VC_PASSWORD=secret' >> ~/.bashrc && export VC_PASSWORD=secret
+echo 'export VC_USER={{user}}' >> ~/.bashrc && export VC_USER={{user}}
+echo 'export VC_PASSWORD={{password}}' >> ~/.bashrc && export VC_PASSWORD={{password}}
 echo 'export VC_BASIC_AUTH=$(htpasswd -nb $VC_USER $VC_PASSWORD)' >> ~/.bashrc && export VC_BASIC_AUTH=$(htpasswd -nb $VC_USER $VC_PASSWORD)
-echo 'export ACME_EMAIL=dev@vicoders.com' >> ~/.bashrc && export ACME_EMAIL=dev@vicoders.com
+echo 'export ACME_EMAIL={{acme_email}}' >> ~/.bashrc && export ACME_EMAIL={{acme_email}}
 echo 'export TRAEFIK_DEBUGE=true' >> ~/.bashrc && export TRAEFIK_DEBUGE=true
 echo 'export ENABLE_TRAEFIK_API=true' >> ~/.bashrc && export ENABLE_TRAEFIK_API=true
 echo 'export ENABLE_TRAEFIK_API_DASHBOARD=true' >> ~/.bashrc && export ENABLE_TRAEFIK_API_DASHBOARD=true
-echo 'export ENABLE_TRAEFIK_CERT_RESOLVER=staging' >> ~/.bashrc && export ENABLE_TRAEFIK_CERT_RESOLVER=staging
-echo 'export ENABLE_TRAEFIK_ACCESS_LOG=true' >> ~/.bashrc && export ENABLE_TRAEFIK_ACCESS_LOG=true
+echo 'export ENABLE_TRAEFIK_CERT_RESOLVER={{cert_resolver}}' >> ~/.bashrc && export ENABLE_TRAEFIK_CERT_RESOLVER={{cert_resolver}}
+echo 'export ENABLE_TRAEFIK_ACCESS_LOG=false' >> ~/.bashrc && export ENABLE_TRAEFIK_ACCESS_LOG=false
 
 touch letsencrypt/acme.json && chmod 600 letsencrypt/acme.json
 touch logs/access.log
